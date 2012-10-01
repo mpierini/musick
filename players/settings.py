3f6b600eb2546561c2a7ab6b92365a74acd169c8
Michele <pierini.michele@gmail.com>
2012-10-01 16:09:12 -0700

    fiddling with db settings & DJANGO_MODULE_SETTINGS

--- players/settings.py
+++ players/settings.py
@@ -9,16 +9,22 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+#setting up db stuff 4 heroku ::
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'sqlite3.db',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
+    'default': dj_database_url.config(default='sqlite3:///db.sqlite3')
 }
+
+#DATABASES = {
+    #'default': {
+        #'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        #'NAME': 'sqlite3.db',                      # Or path to database file if using sqlite3.
+        #'USER': '',                      # Not used with sqlite3.
+        #'PASSWORD': '',                  # Not used with sqlite3.
+        #'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        #'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    #}
+#}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -170,8 +176,8 @@ AUTH_PROFILE_MODULE = "players.UserProfile"
 
 #trying what the man on the internet said about dev env db stuff
 
-import dj_database_url
-import os
+#import dj_database_url
+#import os
 
-if os.getcwd() == "/players":
-    DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
+#if os.getcwd() == "/players":
+    #DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
